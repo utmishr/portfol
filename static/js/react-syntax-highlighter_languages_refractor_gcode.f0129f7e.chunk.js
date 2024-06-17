3bc64e7a2ea78126ff776d5b61838024ebcef77a
Utkarsh <ut2002mishra@gmail.com>
2024-06-17 23:38:58 +0530

    Updates

---
+++ static/js/react-syntax-highlighter_languages_refractor_gcode.f0129f7e.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkportfolio=self.webpackChunkportfolio||[]).push([[9674],{9124:e=>{function o(e){e.languages.gcode={comment:/;.*|\B\(.*?\)\B/,string:{pattern:/"(?:""|[^"])*"/,greedy:!0},keyword:/\b[GM]\d+(?:\.\d+)?\b/,property:/\b[A-Z]/,checksum:{pattern:/(\*)\d+/,lookbehind:!0,alias:"number"},punctuation:/[:*]/}}e.exports=o,o.displayName="gcode",o.aliases=[]}}]);
+//# sourceMappingURL=react-syntax-highlighter_languages_refractor_gcode.f0129f7e.chunk.js.map
---
+++ static/js/react-syntax-highlighter_languages_refractor_hsts.ae5595b9.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkportfolio=self.webpackChunkportfolio||[]).push([[3140],{7761:a=>{function e(a){a.languages.hsts={directive:{pattern:/\b(?:includeSubDomains|max-age|preload)(?=[\s;=]|$)/i,alias:"property"},operator:/=/,punctuation:/;/}}a.exports=e,e.displayName="hsts",e.aliases=[]}}]);
+//# sourceMappingURL=react-syntax-highlighter_languages_refractor_hsts.ae5595b9.chunk.js.map
